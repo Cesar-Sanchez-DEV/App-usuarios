d87968abca169c9e8cfd853f47e5ff6ea869cd3a
Cesar <mekoxplay@gmail.com>
2024-09-19 16:02:54 -0500

    Cambios en Index

--- App usuarios/Models/GestionUserContext.cs
+++ App usuarios/Models/GestionUserContext.cs
@@ -21,8 +21,8 @@ public partial class GestionUserContext : DbContext
     {
         if (!optionsBuilder.IsConfigured)
         {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("server=localhost; database=GestionUser; integrated security=true; Encrypt=false; Trusted_Connection=true;");
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        //        => optionsBuilder.UseSqlServer("server=localhost; database=GestionUser; integrated security=true; Encrypt=false; Trusted_Connection=true;");
         }
     }
 
